d8a641a4bb7f075b9d997f477d99b3c45ae0a663
CodeBlankCjj <cj13480394274@163.com>
2021-06-03 18:31:44 +0800

    点击 icon 切换颜色

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './icon';
 
@@ -25,6 +25,14 @@ const NavWrapper = styled.nav`
           width: 24px;
           height: 24px;
         }
+
+        &.selected {
+          color: #f60;
+
+          .icon {
+            fill: #f60;
+          }
+        }
       }
 
     }
@@ -36,22 +44,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/tags">
+          <NavLink to="/tags" activeClassName="selected">
             <Icon name="tags"/>
             标签
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/money">
+          <NavLink to="/money" activeClassName="selected">
             <Icon name="money"/>
             记账
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/statistics">
+          <NavLink to="/statistics" activeClassName="selected">
             <Icon name="statistics"/>
             统计
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
