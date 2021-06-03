b27b3ff5163ed0267f3dd1ff9b43515e4c0b5414
CodeBlankCjj <cj13480394274@163.com>
2021-06-03 15:14:05 +0800

    导航栏 CSS 初步样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,82 @@
 import React from 'react';
+import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+const Nav = styled.nav`
+  > ul {
+    display: flex;
+
+    > li {
+      width: 33.3%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div>hi</div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Redirect exact from="/" to="money"/>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return <h2>输错地址了！！！！！！</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
 }
 
 export default App;
