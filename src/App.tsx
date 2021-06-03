f4312df3fe2bc1c79e904d19a90b8f145796bd22
CodeBlankCjj <cj13480394274@163.com>
2021-06-03 19:00:05 +0800

    Money.tsx > TagsSection UI 完成

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,33 @@ import Tags from 'views/Tags';
 import Money from 'views/Money';
 import Statistics from 'views/Statistics';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  color: #333333;
+`;
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Redirect exact from="/" to="money"/>
-        <Route path="/tags">
-          <Tags/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="money"/>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
