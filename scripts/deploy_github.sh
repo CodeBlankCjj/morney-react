c8a61ce2eef346ccb9cc72421ab640231c7eabaf
CodeBlankCjj <cj13480394274@163.com>
2021-06-05 15:29:35 +0800

    添加配置文件

--- scripts/deploy_github.sh
+++ scripts/deploy_github.sh
@@ -6,6 +6,6 @@ git init &&
 git add . &&
 git commit -m "deploy" &&
 git remote add origin git@github.com:CodeBlankCjj/morney-react-website.git &&
-git git branch -M main &&
+git branch -M main &&
 git push -u origin main -f
 cd -
